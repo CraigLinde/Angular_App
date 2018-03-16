6d4b61e3659ca18b60d4f8081b515c04b5c94a07
VajkFreyasman <craig.linde.com>
2018-03-16 11:03:02 -0600

    beginning of Angular App

---
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Document } from './document';
+
+@Component({
+  moduleId: module.id,
+  selector: 'documents',
+  templateUrl: 'documents.component.html'
+})
+
+
+export class DocumentsComponent{
+  document: Document[] = [
+  {
+    title: "my first doc",
+    description: 'lksdjflaksdjf',
+    file_url: 'http:google.com',
+    updated_at: '11/11/16',
+    image_url: 'http:google.com',
+  },
+    {
+    title: "my second doc",
+    description: 'lksdjflaksdjf',
+    file_url: 'http:google.com',
+    updated_at: '11/11/16',
+    image_url: 'http:google.com',
+  },
+    {
+    title: "my last doc",
+    description: 'lksdjflaksdjf',
+    file_url: 'http:google.com',
+    updated_at: '11/11/16',
+    image_url: 'http:google.com',
+  }
+  ]
+}
