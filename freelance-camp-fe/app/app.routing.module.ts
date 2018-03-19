15e9f8027235e6cdcce7322d5150993979a9f8aa
VajkFreyasman <craig.linde.com>
2018-03-19 17:08:53 -0600

    implementd Activated routes for find params for show page

--- freelance-camp-fe/app/app.routing.module.ts
+++ freelance-camp-fe/app/app.routing.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -5,6 +6,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
+import { ProposalShowComponent } from './proposal/proposal-show.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -12,6 +14,7 @@ const routes: Routes = [
   { path: 'documents', component: DocumentsComponent },
   { path: 'proposals', component: ProposalListComponent },
   { path: 'proposals/new', component: ProposalNewComponent },
+  { path: 'proposal/:id', component: ProposalShowComponent },
 ]
 
 @NgModule({
@@ -19,6 +22,4 @@ const routes: Routes = [
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
-
-
 
--- freelance-camp-fe/app/proposal/proposal.ts
+++ freelance-camp-fe/app/proposal/proposal.ts
@@ -1,15 +1,16 @@
-export class Proposal{
-  constructor(
-    public id?: number,
-    public customer?: string,
-    public portfolio_url: string = 'http://',
-    public tools?: string,
-    public estimated_hours?: number,
-    public hourly_rate?: number,
-    public weeks_to_complete?: number,
-    public client_email?: string,
-    ){}
 
+export class Proposal {
+    constructor(
+        public id?: number,
+        public customer?: string,
+        public portfolio_url: string = 'http://',
+        public tools?: string,
+        public estimated_hours?: number,
+        public hourly_rate?: number,
+        public weeks_to_complete?: number,
+        public client_email?: string,
+    ) {}
 }
+
 
 //15, 'ABC Company', 'https://CraigLinde.com', 'Ruby On Rails', 150,120,15
